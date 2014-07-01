24156072435c3f366fa12505c415613b23089b4b
Katarzyna Turbiasz <kturbiasz@gmail.com>
2014-07-01 15:10:53 +0200

    css and js finished

--- js/global.js
+++ js/global.js
@@ -1,22 +1,42 @@
-showOrHideTeam = function(evt) {
-  evt.preventDefault();
+// showOrHideTeam = function(evt) {
+//   evt.preventDefault();
+//   var teamSection = document.getElementById("team");
+//   var teamFooter = teamSection.lastElementChild.lastElementChild;
+//   var toggleButton = document.getElementsByClassName("toggleButton")[0];
+//   var window = document.defaultView;
+
+//   if (toggleButton.text == "MORE") {
+//     teamSection.style.height = "1620px";
+//     teamFooter.style.top = "-10px";
+//     toggleButton.innerHTML = "LESS";
+//   } else {
+//     console.log(toggleButton.text);
+//     teamSection.style.height = "860px";
+//     teamFooter.style.top = "-760px";
+//     toggleButton.innerHTML = "MORE";
+//     window.scroll(530, 1300);
+//   };
+//   return false;
+// }
+
+toggleClass = function(e) {
+  e.preventDefault();
   var teamSection = document.getElementById("team");
   var teamFooter = teamSection.lastElementChild.lastElementChild;
   var toggleButton = document.getElementsByClassName("toggleButton")[0];
   var window = document.defaultView;
 
-  if (toggleButton.text == "MORE") {
-    teamSection.style.height = "1600px";
-    teamFooter.style.top = "-10px";
+  if (teamSection.className != 'extend-section') {
+    teamSection.className = "extend-section";
+    teamFooter.className = "extend-footer";
     toggleButton.innerHTML = "LESS";
   } else {
-    console.log(toggleButton.text);
-    teamSection.style.height = "860px";
-    teamFooter.style.top = "-760px";
+    teamSection.className = "";
+    teamFooter.className = "";
     toggleButton.innerHTML = "MORE";
     window.scroll(530, 1300);
   };
   return false;
 }
 
-document.getElementsByClassName("toggleButton")[0].addEventListener("click", showOrHideTeam, true);
+document.getElementsByClassName("toggleButton")[0].addEventListener("click", toggleClass, true);
