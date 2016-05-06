61612c1d510bdf004f87cd47e701ca6728f19bf0
piotrzientara <piotr.zientara@rebased.pl>
2016-05-06 11:47:57 +0200

    refactoring of js

--- js/global.js
+++ js/global.js
@@ -1,107 +1,93 @@
-$('document').ready(function() {
+var rebased = {
 
-  function hideOrShowTeam() {
-    var wholeSection = $("#team");
+  init: function() {
+    $('.menu-controls').on('click', function() {
+      event.stopPropagation();
+      rebased.toggleMenu();
+    })
+    $("#team").on("click", ".toggle-button", rebased.hideOrShowTeam);
+    $("#team").on("click", ".divider-button", rebased.hideOrShowTeam);
+    $(window).on('scroll', rebased.scrollSite);
+    if($('#main_page').length === 1) {
+      $(window).on('scroll', rebased.scrollSpy);
+    }
+  },
 
-    var sectionToToggle = wholeSection.children('section');
-    var teamDividerLine = wholeSection.children().last();
-    var teamFooter = sectionToToggle.children().last();
-    var moreLessButton = teamFooter.children().last();
-    var triangleButtonBackground = wholeSection.children('div');
+  mobileMenuHeight: ( 54 * $('.mobile-menu-links li').length ) + "px",
 
-    if (sectionToToggle.attr('class') == "to-toggle") {
+  toggleExtendedTeamElements: function(wholeSection) {
+    var sectionToToggle = wholeSection.children('section'),
+        teamDividerLine = wholeSection.children().last(),
+        triangleButtonBackground = wholeSection.children('div');
 
-      $(sectionToToggle[0]).toggleClass("to-toggle extend-section");
-      teamDividerLine.toggleClass("divider-line extend-divider-line");
-      teamFooter.addClass("extend-footer");
-      triangleButtonBackground.toggleClass("divider-button-background extend-divider-button-background");
-      moreLessButton[0].innerHTML = "LESS";
+    $(sectionToToggle[0]).toggleClass("to-toggle extend-section");
+    teamDividerLine.toggleClass("divider-line extend-divider-line");
+    triangleButtonBackground.toggleClass("divider-button-background extend-divider-button-background");
+  },
 
-    } else {
+  hideOrShowTeam: function() {
+    var wholeSection = $("#team"),
+        sectionToToggle = wholeSection.children('section');
 
-      $(sectionToToggle[0]).toggleClass("extend-section to-toggle");
-      teamDividerLine.toggleClass("extend-divider-line divider-line");
-      teamFooter.removeClass("extend-footer");
-      triangleButtonBackground.toggleClass("extend-divider-button-background divider-button-background");
-      moreLessButton[0].innerHTML = "MORE";
+    rebased.toggleExtendedTeamElements(wholeSection);
+    if (sectionToToggle.attr('class') === "to-toggle") {
       $('html, body').animate({
         scrollTop: 630
       }, 900);
     };
-
     return false;
-  }
+  },
 
-  function scrollSite() {
+  scrollSite: function() {
     var header = $('body').children().first();
     if (window.pageYOffset > 20) {
       header[0].className = "visible";
     } else {
       header[0].className = "";
     };
-  }
+  },
 
-  function scrollSpy() {
-    var currentActive = $('.active-section') || $('nav');
-    var position = window.pageYOffset;
-    var main_page = $('#main_page');
-    if(main_page.length === 1) {
-      var sectionTeamEnter = $('#team').position().top;
-      var sectionProjectsEnter = $('#projects').position().top;
-      var sectionOpenSourceEnter = $('#open-source').position().top;
-      var sectionCommunityEnter = $('#community').position().top;
-      var sectionContactEnter = $('#contact').position().top;
-      var sectionTeamExit = sectionProjectsEnter;
-      var sectionProjectsExit = sectionOpenSourceEnter;
-      var sectionOpenSourceExit = sectionCommunityEnter;
-      var sectionCommunityExit = sectionContactEnter;
+  showHoverOnCurrentTab: function(tab) {
+    $('.active-section').removeClass('active-section');
+    tab.next().addClass('active-section');
+  },
 
-      if (position >= sectionTeamEnter && position < sectionTeamExit) {
-        currentActive.removeClass('active-section');
-        $('a[href="#team"]').next().addClass('active-section');
-      } else if (position >= sectionProjectsEnter && position < sectionProjectsExit) {
-        currentActive.removeClass('active-section');
-        $('a[href="#projects"]').next().addClass('active-section');
-      } else if (position >= sectionOpenSourceEnter && position < sectionOpenSourceExit) {
-        currentActive.removeClass('active-section');
-        $('a[href="#open-source"]').next().addClass('active-section');
-      } else if (position >= sectionCommunityEnter && position < sectionCommunityExit) {
-        currentActive.removeClass('active-section');
-        $('a[href="#community"]').next().addClass('active-section');
-      } else if (position >= sectionContactEnter) {
-        currentActive.removeClass('active-section');
-        $('a[href="#contact"]').next().addClass('active-section');
-      };
-    }
-  }
+  scrollSpy: function() {
+    var position = window.pageYOffset,
+        sectionTeamEnter = $('#team').position().top,
+        sectionProjectsEnter = $('#projects').position().top,
+        sectionOpenSourceEnter = $('#open-source').position().top,
+        sectionCommunityEnter = $('#community').position().top,
+        sectionContactEnter = $('#contact').position().top;
+    var sectionTeamExit = sectionProjectsEnter,
+        sectionProjectsExit = sectionOpenSourceEnter,
+        sectionOpenSourceExit = sectionCommunityEnter,
+        sectionCommunityExit = sectionContactEnter;
 
-  var linkHeight = 54,
-      numberOfLinks = $('.mobile-menu-links li').length,
-      mobileMenuLinksHeight = linkHeight * numberOfLinks + "px";
+    if (position >= sectionTeamEnter && position < sectionTeamExit) {
+      rebased.showHoverOnCurrentTab($('a[href="#team"]'));
+    } else if (position >= sectionProjectsEnter && position < sectionProjectsExit) {
+      rebased.showHoverOnCurrentTab($('a[href="#projects"]'));
+    } else if (position >= sectionOpenSourceEnter && position < sectionOpenSourceExit) {
+      rebased.showHoverOnCurrentTab($('a[href="#open-source"]'));
+    } else if (position >= sectionCommunityEnter && position < sectionCommunityExit) {
+      rebased.showHoverOnCurrentTab($('a[href="#community"]'));
+    } else if (position >= sectionContactEnter) {
+      rebased.showHoverOnCurrentTab($('a[href="#contact"]'));
+    };
+  },
 
-  function toggleMenu() {
-    if ($('.mobile-menu-links').css('height') == mobileMenuLinksHeight) {
+  toggleMenu: function() {
+    if ($('.mobile-menu-links').css('height') == rebased.mobileMenuHeight) {
       $('.mobile-menu-links').css('height', '0px')
     } else {
-      $('.mobile-menu-links').css('height', mobileMenuLinksHeight)
+      $('.mobile-menu-links').css('height', rebased.mobileMenuHeight)
       $(this).css('border-radius', '0')
     }
     return
   }
+}
 
-  $('.menu-controls').on('click', function() {
-    event.stopPropagation();
-    toggleMenu();
-  })
-
-  $('body').click(function(e) {
-    if ($('.mobile-menu-links').css('height') == mobileMenuLinksHeight) {
-      toggleMenu();
-    }
-  });
-
-  $("#team").on("click", ".toggle-button", hideOrShowTeam);
-  $("#team").on("click", ".divider-button", hideOrShowTeam);
-  $(window).on('scroll', scrollSite);
-  $(window).on('scroll', scrollSpy);
+$('document').ready(function() {
+  rebased.init();
 });
