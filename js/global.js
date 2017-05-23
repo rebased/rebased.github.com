618bd1cc68f00c798746f173c299ca00604761db
Kasia Jarmołkowicz <kasia.jarmolkowicz@gmail.com>
2017-05-23 14:36:43 +0200

    Remove jQuery from global.js

--- js/global.js
+++ js/global.js
@@ -1,63 +1,19 @@
-var rebased = {
+// toggle menu on mobile
+var toggleMenu = function() {
+  document.body.classList.toggle('noscroll');
+  document.getElementById('nav').classList.toggle('active');
+}
 
-  init: function() {
-    $("#hamburger").click(function() {
-      $("#menu").slideToggle("slow", function() {
-        $("#hamburger").toggleClass("expanded");
-        $("body").toggleClass("noscroll");
-        $(this).toggleClass("full-list").removeAttr("style");
-      });
-    });
+document.getElementById('nav').addEventListener('click', toggleMenu);
 
-    $(window).resize(function() {
-      $("#hamburger").removeClass("expanded");
-      $("body").removeClass("noscroll");
-      $("#menu").removeClass("full-list").removeAttr("style");
-    });
+// toggle team list
+var toggleTeamList = function(e) {
+  e.preventDefault();
+  document.getElementById('team').classList.toggle('extended');
 
-    $(".menu-items li a").click(function() {
-      $("body").toggleClass("noscroll");
-
-      if($(window).width() < 992) {
-        $("#menu").slideToggle( "slow", function() {
-          $("#hamburger").toggleClass("expanded");
-        });
-      };
-
-      var section = $(this).attr('href')
-      $("html,body").animate({
-        scrollTop: $(section).offset().top -85 },
-        'slow');
-    });
-
-    $("#team").on("click", ".toggle-button", rebased.hideOrShowTeam);
-    $("#team").on("click", ".divider-button", rebased.hideOrShowTeam);
-  },
-
-  toggleExtendedTeamElements: function(wholeSection) {
-    var sectionToToggle = wholeSection.children('section'),
-        teamDividerLine = wholeSection.children().last(),
-        triangleButtonBackground = wholeSection.children('div');
-
-    $(sectionToToggle[0]).toggleClass("to-toggle extend-section");
-    teamDividerLine.toggleClass("divider-line extend-divider-line");
-    triangleButtonBackground.toggleClass("divider-button-background extend-divider-button-background");
-  },
-
-  hideOrShowTeam: function() {
-    var wholeSection = $("#team"),
-        sectionToToggle = wholeSection.children('section');
-
-    rebased.toggleExtendedTeamElements(wholeSection);
-    if (sectionToToggle.attr('class') === "to-toggle") {
-      $('html, body').animate({
-        scrollTop: wholeSection.offset().top -85
-      }, 'slow');
-    };
-    return false;
+  if (!document.getElementById('team').classList.contains('extended')) {
+    document.getElementById('team').scrollIntoView()
   }
 }
 
-$('document').ready(function() {
-  rebased.init();
-});
+document.getElementById('team-list-toggle').addEventListener('click', toggleTeamList);
